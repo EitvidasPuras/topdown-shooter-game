8c852d6e06a049b57932d61ad4091a61342f5b58
Eitvidas Puras <peitvis@gmail.com>
2018-11-07 17:38:01 +0200

    Fixed the infamous Fake Main Class

--- WindowsFormsApp1/MainForm.cs
+++ WindowsFormsApp1/MainForm.cs
@@ -19,6 +19,8 @@ namespace GameClient
         RequestsController requestController = new RequestsController();
         Player myPlayer = new Player();
         String url1 = "";
+        List<GameServer.Models.Player> listOfPlayers = new List<GameServer.Models.Player>();
+        List<GameServer.Interfaces.ISkin> listOfDrawers = new List<GameServer.Interfaces.ISkin>();
         public MainForm()
         {
             InitializeComponent();
