51f85efd6e5dde601b65f01fd685d5fd27ba00c8
outoflock377 <outoflock377@gmail.com>
2018-11-27 19:22:23 +0200

    Added wpf project, work in progress

---
+++ GameClientWpf/MainWindow.xaml.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GameClient;
+using GameClient.Models;
+using GameClientWpf;
+using GameServer.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int moveRightDist = 0;
+        private int moveDownDist = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveRight(object sender, RoutedEventArgs e)
+        {
+
+            this.moveRightDist++;
+            this.moveDownDist++;
+            crosshair.RenderTransform = new TranslateTransform(this.moveRightDist, this.moveDownDist);
+
+        }
+
+        private void LayoutRoot_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        //private void Window_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    switch (e.Key)
+        //    {
+        //        case Key.W:
+        //            moveDownDist -= 15;
+
+        //            break;
+        //        case Key.A:
+        //            moveRightDist -= 15;
+
+        //            break;
+        //        case Key.S:
+        //            moveDownDist += 15;
+
+        //            break;
+        //        case Key.D:
+        //            moveRightDist += 15;
+
+        //            break;
+        //            //case 'x':
+        //            //    var statusCode = await requestController.DeletePlayerAsync(myPlayer.Id);
+        //            //    this.Close();
+        //    }
+        //    //crosshair2.RenderTransform = new TranslateTransform(this.moveRightDist, this.moveDownDist);
+        //    image.RenderTransform = new TranslateTransform(this.moveRightDist, this.moveDownDist);
+        //}
+
+
+        //private System.Drawing.Graphics formGraphics;
+        //private System.Drawing.Graphics formGraphics2;
+
+        RequestsController requestController = new RequestsController();
+        Player myPlayer = new Player();
+        String url1 = "";
+        List<GameServer.Models.Player> listOfPlayers = new List<GameServer.Models.Player>();
+        List<GameServer.Models.Weapon> listOfWeapons = new List<GameServer.Models.Weapon>();
+        List<GameServer.Interfaces.ISkin> listOfDrawers = new List<GameServer.Interfaces.ISkin>();
+        ObserverController observer = new ObserverController();
+
+        // pagetina visus playerius ir juos atvaizduoja, vėliau sukuria playeri esančiam klientui
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            Console.WriteLine("0)\tGet all player");
+            //requestController.client.BaseAddress = new Uri("https://topdown-shooter.azurewebsites.net/");
+            requestController.client.BaseAddress = new Uri("http://localhost:47850/");
+            requestController.client.DefaultRequestHeaders.Accept.Clear();
+            requestController.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(requestController.mediaType));
+
+            ICollection<Player> playersList = await requestController.GetAllPlayerAsync(requestController.client.BaseAddress.PathAndQuery);
+
+            Random rnd = new Random();
+
+
+            foreach (Player p in playersList)
+            {
+                listOfPlayers.Add(p);
+                //Coordinates coordinates = new Coordinates
+                //{
+                //    Id = p.Id,
+                //    PosX = rnd.Next(10, this.Width),
+                //    PosY = rnd.Next(10, this.Height)
+                //};
+                //var patchStatusCode = await requestController.PatchPlayerAsync(coordinates);
+                this.Form1_PaintPlayer((int)p.PosX, (int)p.PosY);
+            }
+
+            // Create a new player
+            Console.WriteLine("1.1)\tCreate the player");
+            myPlayer = new Player
+            {
+                Name = "Studentas-" + playersList.Count.ToString(),
+                Score = 100,
+                PosX = rnd.Next(10, (int)this.Width),
+                PosY = rnd.Next(10, (int)this.Height)
+            };
+
+            var url = await requestController.CreatePlayerAsync(myPlayer);
+
+            url1 = url.PathAndQuery;
+            myPlayer = await requestController.GetPlayerAsync(url.PathAndQuery);
+            listOfPlayers.Add(myPlayer);
+
+            this.Form1_PaintPlayer((int)myPlayer.PosX, (int)myPlayer.PosY);
+
+            playersList = await requestController.GetAllPlayerAsync(requestController.client.BaseAddress.PathAndQuery);
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += timer1_TickAsync;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+            //timer1.Enabled = true;
+        }
+
+        // paint dot
+        private void Form1_PaintPlayer(int x, int y)
+        {
+            //Image image = Image.FromFile(@"..\\..\\assets\\basicPlayer.png");
+            //formGraphics.DrawImage(image, x, y);
+            //formGraphics.DrawImage()
+        }
+
+        private void Form1_PaintWeapons(int x, int y, string weaponName)
+        {
+            if (weaponName.Contains("AK47"))
+            {
+                //Image image = Image.FromFile(@"..\\..\\assets\\ak47.png");
+                //formGraphics.DrawImage(image, x, y);
+            }
+            else if (weaponName.Contains("M4A1"))
+            {
+                //Image image = Image.FromFile(@"..\\..\\assets\\m4a1.png");
+                //formGraphics.DrawImage(image, x, y);
+            }
+            else if (weaponName.Contains("DesertEagle"))
+            {
+                //Image image = Image.FromFile(@"..\\..\\assets\\deserteagle.png");
+                //formGraphics.DrawImage(image, x, y);
+            }
+            else if (weaponName.Contains("P250"))
+            {
+                //Image image = Image.FromFile(@"..\\..\\assets\\p250.png");
+                //formGraphics.DrawImage(image, x, y);
+            }
+            else if (weaponName.Contains("Grenade"))
+            {
+                //Image image = Image.FromFile(@"..\\..\\assets\\grenade.png");
+                //formGraphics.DrawImage(image, x, y);
+            }
+        }
+
+        //deletint playeri?
+        //private async void Form1_FormClosingAsync(object sender, FormClosingEventArgs e)
+        //{
+        //    //Delete the player
+        //    //Console.WriteLine("4.3)\tDelete the player");
+        //    //var statusCode = await requestController.DeletePlayerAsync(myPlayer.Id);
+        //    //Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+        //}
+
+        //timeris getina visus playerius ir atvaizduoja pagal koordinates
+        private async void timer1_TickAsync(object sender, EventArgs e)
+        {
+
+            bool updated = await observer.CheckIfChangedAsync(requestController.client.BaseAddress.ToString(), myPlayer);
+            if (updated)
+            {
+                //this.Invalidate();
+                ICollection<Player> playersList = await requestController.GetAllPlayerAsync(requestController.client.BaseAddress.PathAndQuery);
+                ICollection<Weapon> weaponsList = await requestController.GetAllWeaponsAsync(requestController.client.BaseAddress.PathAndQuery);
+
+                foreach (Player p in playersList)
+                {
+                    Player oldPlayer = listOfPlayers.Find(i => i.Id == p.Id);
+                    if (oldPlayer != null)
+                    {
+                        if (!oldPlayer.checkEquality(p))
+                        {
+                            if (p.Id == myPlayer.Id)
+                            {
+                                //Image image = Image.FromFile(@"..\\..\\assets\\empty.png");
+                                //formGraphics.DrawImage(image, (int)oldPlayer.PosX, (int)oldPlayer.PosY);
+
+                                this.Form1_PaintPlayer((int)p.PosX, (int)p.PosY);
+                            }
+                            else
+                            {
+                                //Image image = Image.FromFile(@"..\\..\\assets\\empty.png");
+                                //formGraphics.DrawImage(image, (int)oldPlayer.PosX, (int)oldPlayer.PosY);
+
+                                this.Form1_PaintPlayer((int)p.PosX, (int)p.PosY);
+                            }
+
+                            int index = listOfPlayers.FindIndex(i => i.Id == p.Id);
+                            listOfPlayers[index] = p;
+                        }
+                    }
+                }
+
+
+                foreach (Weapon w in weaponsList)
+                {
+                    this.Form1_PaintWeapons((int)w.PosX, (int)w.PosY, w.Name);
+                }
+            }
+        }
+
+        //reaguoja į paspaustus mygtukus
+        private async void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            //atnaujina kliento objektą iš duombazes
+            myPlayer = await requestController.GetPlayerAsync(url1);
+            Coordinates coordinates = new Coordinates
+            {
+                Id = myPlayer.Id,
+                PosX = myPlayer.PosX,
+                PosY = myPlayer.PosY
+            };
+            var patchStatusCode = (Object)null;
+
+            switch (e.Key)
+            {
+                case Key.W:
+                    coordinates.PosY -= 15;
+                    patchStatusCode = await requestController.PatchPlayerAsync(coordinates);
+                    break;
+                case Key.A:
+                    coordinates.PosX -= 15;
+                    patchStatusCode = await requestController.PatchPlayerAsync(coordinates);
+                    break;
+                case Key.S:
+                    coordinates.PosY += 15;
+                    patchStatusCode = await requestController.PatchPlayerAsync(coordinates);
+                    break;
+                case Key.D:
+                    coordinates.PosX += 15;
+                    patchStatusCode = await requestController.PatchPlayerAsync(coordinates);
+                    break;
+                case Key.X:
+                    var statusCode = await requestController.DeletePlayerAsync(myPlayer.Id);
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Pen pen = new Pen(Color.Azure);
+            //formGraphics.DrawLine(pen, e.X, e.Y, myPlayer.PosX + 25, myPlayer.PosY + 25);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Pen pen = new Pen(Color.Black);
+            //formGraphics.DrawLine(pen, e.X, e.Y, myPlayer.PosX + 25, myPlayer.PosY + 25);
+            //this.Invalidate()
+        }
+    }
+}
---
+++ GameClientWpf/RequestsController.cs
@@ -0,0 +1,242 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GameClient.Models;
+using GameServer.Models;
+
+namespace GameClientWpf
+{
+    class RequestsController
+    {
+        public HttpClient client = new HttpClient();
+        public string requestUri = "api/player/";
+        public string mediaType = "application/json";
+
+        public void ShowProduct(Player player)
+        {
+            Console.WriteLine($"Id: {player.Id}\tName: {player.Name}\tScore: " +
+                              $"{player.Score}\tposX: {player.PosX}\tposY: {player.PosY}");
+        }
+
+        public async Task<Uri> CreatePlayerAsync(Player player)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                requestUri, player);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            Player player2 = await response.Content.ReadAsAsync<Player>();
+            if (player2 != null)
+            {
+                ShowProduct(player2);
+            }
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        public async Task<ICollection<Player>> GetAllPlayerAsync(string path)
+        {
+            ICollection<Player> players = null;
+            HttpResponseMessage response = await client.GetAsync(path + "api/player");
+            if (response.IsSuccessStatusCode)
+            {
+                players = await response.Content.ReadAsAsync<ICollection<Player>>();
+            }
+            return players;
+        }
+
+        public async Task<ICollection<Weapon>> GetAllWeaponsAsync(string path)
+        {
+            ICollection<object> weps = null;
+            ICollection<Weapon> weapons = new List<Weapon>();
+            HttpResponseMessage response = await client.GetAsync(path + "api/weapon");
+            if (response.IsSuccessStatusCode)
+            {
+                weps = await response.Content.ReadAsAsync<ICollection<object>>();
+            }
+
+            foreach (object wep in weps)
+            {
+                Weapon weapon = null;
+                Newtonsoft.Json.Linq.JObject obj = JsonConvert.DeserializeObject<dynamic>(wep.ToString());
+                string name = obj.GetValue("name").ToString();
+
+                if (name.Contains("AK47"))
+                    weapon = JsonConvert.DeserializeObject<AK47>(wep.ToString());
+                else if (name.Contains("M4A1"))
+                    weapon = JsonConvert.DeserializeObject<M4A1>(wep.ToString());
+                else if(name.Contains("DesertEagle"))
+                    weapon = JsonConvert.DeserializeObject<DesertEagle>(wep.ToString());
+                else if(name.Contains("P250"))
+                    weapon = JsonConvert.DeserializeObject<P250>(wep.ToString());
+                else if(name.Contains("Grenade"))
+                    weapon = JsonConvert.DeserializeObject<GrenadeAdapter>(wep.ToString());
+                weapons.Add(weapon);
+            }
+            return weapons;
+        }
+
+        public async Task<Player> GetPlayerAsync(string path)
+        {
+            Player player = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                player = await response.Content.ReadAsAsync<Player>();
+            }
+            return player;
+        }
+
+        public async Task<HttpStatusCode> UpdatePlayerAsync(Player player)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                requestUri + $"{player.Id}", player);
+            response.EnsureSuccessStatusCode();
+
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> PatchPlayerAsync(Coordinates coordinates)
+        {
+            string jsonString = JsonConvert.SerializeObject(coordinates);    //"{\"id\":1, \"posX\":777,\"posY\":777}";
+
+            HttpContent httpContent = new StringContent(jsonString, System.Text.Encoding.UTF8, mediaType);
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = new HttpMethod("PATCH"),
+                RequestUri = new Uri(client.BaseAddress + requestUri),
+                Content = httpContent,
+            };
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            return response.StatusCode;
+
+        }
+
+        public async Task<HttpStatusCode> DeletePlayerAsync(long id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                requestUri + $"{id}");
+            return response.StatusCode;
+        }
+
+        public void getLogInformationAsync()
+        {
+
+        }
+
+        ////static void Main()
+        ////{
+        ////    Console.WriteLine("Web API Client says: \"Hello World!\"");
+        ////    RunAsync().GetAwaiter().GetResult();
+        ////}
+
+        ////static async Task RunAsync()
+        ////{
+        ////    // Update port # in the following line.
+        ////    //client.BaseAddress = new Uri("https://gameserver.azurewebsites.net/"); //api /player/");
+        ////    client.BaseAddress = new Uri("https://localhost:44371/");
+        ////    client.DefaultRequestHeaders.Accept.Clear();
+        ////    client.DefaultRequestHeaders.Accept.Add(
+        ////        new MediaTypeWithQualityHeaderValue(mediaType));
+
+        ////    try
+        ////    {
+        ////        // Get all players
+        ////        Console.WriteLine("0)\tGet all player");
+        ////        ICollection<Player> playersList = await GetAllPlayerAsync(client.BaseAddress.PathAndQuery);
+        ////        foreach (Player p in playersList)
+        ////        {
+        ////            ShowProduct(p);
+        ////        }
+
+
+        ////        // Create a new player
+        ////        Console.WriteLine("1.1)\tCreate the player");
+        ////        Player player = new Player
+        ////        {
+        ////            Name = "Studentas-" + playersList.Count.ToString(),
+        ////            Score = 100,
+        ////            PosX = 20,
+        ////            PosY = 30
+        ////        };
+
+        ////        var url = await CreatePlayerAsync(player);
+        ////        Console.WriteLine($"Created at {url}");
+
+        ////        // Get the created player
+        ////        Console.WriteLine("1.2)\tGet created player");
+        ////        player = await GetPlayerAsync(url.PathAndQuery);
+        ////        ShowProduct(player);
+
+        ////        Console.WriteLine("2.1)\tFull Update the player's score");
+        ////        player.Score = 80;
+        ////        var updateStatusCode = await UpdatePlayerAsync(player);
+        ////        Console.WriteLine($"Updated (HTTP Status = {(int)updateStatusCode})");
+
+        ////        // Get the full updated player
+        ////        Console.WriteLine("2.2)\tGet updated the player");
+        ////        player = await GetPlayerAsync(url.PathAndQuery);
+        ////        ShowProduct(player);
+
+        ////        //Partial update - patch - of the player
+        ////        Console.WriteLine("3.1)\tPatch Update the player's score");
+        ////        Coordinates coordinates = new Coordinates
+        ////        {
+        ////            Id = player.Id,
+        ////            PosX = player.PosX + 10,
+        ////            PosY = player.PosY + 15
+        ////        };
+        ////        var patchStatusCode = await PatchPlayerAsync(coordinates);
+        ////        Console.WriteLine($"Patched (HTTP Status = {(int)patchStatusCode})");
+
+        ////        // Get the patched  player
+        ////        Console.WriteLine("3.2)\tGet patched player");
+        ////        player = await GetPlayerAsync(url.PathAndQuery);
+        ////        ShowProduct(player);
+
+        ////        //Create player for deletion
+        ////        Console.WriteLine("4.1)\tCreate the player for deletion");
+        ////        Player delPlayer = new Player
+        ////        {
+        ////            Name = "StudentasDel-" + (playersList.Count + 1).ToString(),
+        ////            Score = 444,
+        ////            PosX = 444,
+        ////            PosY = 444
+        ////        };
+        ////        var url4Del = await CreatePlayerAsync(delPlayer);
+        ////        Console.WriteLine($"Created at {url4Del}");
+
+        ////        //Show created player for deletion
+        ////        Console.WriteLine("4.2)\tGet the player created for deletion ");
+        ////        delPlayer = await GetPlayerAsync(url4Del.PathAndQuery);
+        ////        ShowProduct(delPlayer);
+
+        ////        // Delete the player
+        ////        Console.WriteLine("4.3)\tDelete the player");
+        ////        var statusCode = await DeletePlayerAsync(delPlayer.Id);
+        ////        Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+        ////        //check if deletion was successful
+        ////        Console.WriteLine("4.4)\tCheck if deletion was successful");
+        ////        delPlayer = await GetPlayerAsync(url4Del.PathAndQuery);
+        ////        ShowProduct(delPlayer);
+
+        ////        Console.WriteLine("Web API Client says: \"GoodBy!\"");
+
+        ////    }
+        ////    catch (Exception e)
+        ////    {
+        ////        Console.WriteLine(e.Message);
+        ////    }
+
+        ////    Console.ReadLine();
+        ////}
+    }
+}
